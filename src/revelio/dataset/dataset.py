a01dbcf8136cb5029d4e0afaba86b8a4ec09a8e4
ndido98 <ndido98@gmail.com>
2022-09-14 14:31:55 +0200

    chore(dataset): make dataset fields private

--- src/revelio/dataset/dataset.py
+++ src/revelio/dataset/dataset.py
@@ -31,21 +31,24 @@ class Dataset(IterableDataset):
         augmentation_steps: list[AugmentationStep],
         feature_extractor: None,  # TODO: add type
     ) -> None:
-        self.paths = paths
-        self.face_detector = face_detector
-        self.augmentation_steps = augmentation_steps
-        self.feature_extractor = feature_extractor
+        self._paths = paths
+        self._face_detector = face_detector
+        self._augmentation_steps = augmentation_steps
+        self._feature_extractor = feature_extractor
 
     def __iter__(self) -> Iterator[dict[str, Any]]:
         # Offline preprocessing
         elems = self._get_elems_iterator()
         for elem in elems:
             elem = _element_with_images(elem)
-            if self.face_detector is not None:
-                elem = self.face_detector.process(elem)
+            if self._face_detector is not None:
+                elem = self._face_detector.process(elem)
             # Feature extraction is offline only if augmentation is disabled
-            if len(self.augmentation_steps) == 0 and self.feature_extractor is not None:
-                elem = self.feature_extractor.process(elem)
+            if (
+                len(self._augmentation_steps) == 0
+                and self._feature_extractor is not None
+            ):
+                elem = self._feature_extractor.process(elem)
             # Now we can close the images, as we don't need them anymore in the offline
             # preprocessing phase; this way we avoid opening them twice
             for x in elem.x:
@@ -56,17 +59,17 @@ class Dataset(IterableDataset):
             elem = _element_with_images(elem)
             # We can safely call the face detector again, as this time it will load
             # the precomputed bounding boxes and landmarks
-            if self.face_detector is not None:
-                elem = self.face_detector.process(elem)
+            if self._face_detector is not None:
+                elem = self._face_detector.process(elem)
             # Augment the dataset; this is always done online
-            for step in self.augmentation_steps:
+            for step in self._augmentation_steps:
                 elem = step.process(elem)
-            if self.feature_extractor is not None:
+            if self._feature_extractor is not None:
                 # If augmentation is enabled, feature extraction is done online;
                 # otherwise, we load the precomputed features
-                elem = self.feature_extractor.process(
+                elem = self._feature_extractor.process(
                     elem,
-                    force_online=len(self.augmentation_steps) > 0,
+                    force_online=len(self._augmentation_steps) > 0,
                 )
             yield {
                 "x": [
@@ -83,9 +86,9 @@ class Dataset(IterableDataset):
     def _get_elems_iterator(self) -> Iterator[DatasetElement]:
         worker_info = get_worker_info()
         if worker_info is None:
-            for p in self.paths:
+            for p in self._paths:
                 yield p
         else:
-            for i, p in enumerate(self.paths):
+            for i, p in enumerate(self._paths):
                 if i % worker_info.num_workers == worker_info.id:
                     yield p
