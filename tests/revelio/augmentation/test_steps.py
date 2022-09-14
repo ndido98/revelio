c8974d3d11e9169e6aa0e900cc3f4c284498c271
ndido98 <ndido98@gmail.com>
2022-09-14 14:13:42 +0200

    test(augmentation): add augmentation tests

---
+++ tests/revelio/augmentation/test_steps.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+from unittest import mock
+
+import pytest
+from PIL import Image
+from PIL.ImageOps import invert
+
+from revelio.augmentation.step import AugmentationStep
+from revelio.dataset.element import DatasetElement, ElementClass, ElementImage
+
+
+class Invert(AugmentationStep):
+    def process_element(self, elem: DatasetElement) -> DatasetElement:
+        new_xs = []
+        for x in elem.x:
+            assert x.image is not None
+            new_x = ElementImage(
+                path=x.path,
+                image=invert(x.image),
+            )
+            new_xs.append(new_x)
+        return DatasetElement(
+            original_dataset=elem.original_dataset,
+            x=tuple(new_xs),
+            y=elem.y,
+        )
+
+
+@pytest.fixture
+def dataset_element() -> DatasetElement:
+    black = Image.new("RGB", (1, 1), "black")
+    img = ElementImage(path=Path("test"), image=black)
+    return DatasetElement(
+        original_dataset="test",
+        x=(img, img),
+        y=ElementClass.BONA_FIDE,
+    )
+
+
+def is_black(img: ElementImage) -> bool:
+    assert img.image is not None
+    return img.image.getpixel((0, 0)) == (0, 0, 0)  # type: ignore
+
+
+def is_white(img: ElementImage) -> bool:
+    print(img.image)
+    assert img.image is not None
+    return img.image.getpixel((0, 0)) == (255, 255, 255)  # type: ignore
+
+
+def test_probability_0(dataset_element: DatasetElement) -> None:
+    step = Invert(probability=0.0)
+    for _ in range(1000):
+        processed = step.process(dataset_element)
+        for img in processed.x:
+            assert is_black(img)
+
+
+def test_probability_1(dataset_element: DatasetElement) -> None:
+    step = Invert(probability=1.0)
+    for _ in range(1000):
+        processed = step.process(dataset_element)
+        for img in processed.x:
+            assert is_white(img)
+
+
+def test_probability_0_5(dataset_element: DatasetElement) -> None:
+    step = Invert(probability=0.5)
+    black_count = 0
+    white_count = 0
+    attempts = 1000
+    # Chosen by fair dice roll. Guaranteed to be random.
+    with mock.patch("random.random", return_value=0.4):
+        for _ in range(attempts):
+            processed = step.process(dataset_element)
+            for img in processed.x:
+                if is_black(img):
+                    black_count += 1
+                elif is_white(img):
+                    white_count += 1
+                else:
+                    pytest.fail()
+        assert black_count == 0
+        assert white_count == attempts * 2
