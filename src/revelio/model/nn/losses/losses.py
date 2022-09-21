56992b38950b15c62e4612c10964376e9d0b8aeb
ndido98 <ndido98@gmail.com>
2022-09-21 10:47:29 +0200

    feat(losses): add adam

--- src/revelio/model/nn/losses/losses.py
+++ src/revelio/model/nn/losses/losses.py
@@ -8,3 +8,8 @@ from .loss import Loss
 class BCEWithLogitsLoss(Loss):
     def get(self, **kwargs: Any) -> torch.nn.Module:
         return torch.nn.BCEWithLogitsLoss(**kwargs)
+
+
+class BCELoss(Loss):
+    def get(self, **kwargs: Any) -> torch.nn.Module:
+        return torch.nn.BCELoss(**kwargs)
