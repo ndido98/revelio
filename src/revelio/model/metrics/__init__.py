2ebd8627d59d2da443a1ba8a062446265b4cda29
ndido98 <ndido98@gmail.com>
2022-09-27 15:01:58 +0200

    feat(metrics): add accuracy

--- src/revelio/model/metrics/__init__.py
+++ src/revelio/model/metrics/__init__.py
@@ -1,1 +1,2 @@
+from .accuracy import *
 from .metric import *
---
+++ src/revelio/model/metrics/accuracy.py
@@ -0,0 +1,27 @@
+from typing import Any
+
+import torch
+
+from .metric import Metric
+
+
+class Accuracy(Metric):
+    @property
+    def name(self) -> str:
+        return "accuracy"
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(**kwargs)
+        self.reset()
+
+    def update(self, y_pred: torch.Tensor, y_true: torch.Tensor) -> None:
+        pred_classes = torch.round(y_pred)
+        self.total += y_true.shape[0]
+        self.correct += (pred_classes == y_true).sum()
+
+    def compute(self) -> torch.Tensor:
+        return self.total / self.correct
+
+    def reset(self) -> None:
+        self.total = torch.zeros((), device=self.device)
+        self.correct = torch.zeros((), device=self.device)
