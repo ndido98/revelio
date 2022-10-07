6de92b8cd85776d68e489d5c788e5cd3eae8f4d1
ndido98 <ndido98@gmail.com>
2022-10-07 10:38:57 +0200

    feat(loaders): add morphdb loader

--- src/revelio/dataset/loaders/loaders.py
+++ src/revelio/dataset/loaders/loaders.py
@@ -32,3 +32,31 @@ class PMDBLoader(DatasetLoader):
                     )
                 )
         return bona_fide + morphed
+
+
+class MorphDBLoader(DatasetLoader):
+    def load(self, path: Path) -> list[DatasetElementDescriptor]:
+        all_images = sorted(path.rglob("*.png"))
+        bona_fide = []
+        morphed = []
+        for image in all_images:
+            if "morph" not in image.name and "keypoints" not in image.name:
+                bona_fide.append(
+                    DatasetElementDescriptor(
+                        x=(image,),
+                        y=ElementClass.BONA_FIDE,
+                    )
+                )
+        morphed_images = sorted(path.rglob("morph*.png"))
+        # Take at most 4 morphed images
+        for _, g in itertools.groupby(morphed_images, lambda x: x.stem[:-4]):
+            group_images = list(g)
+            morphed_count = min(4, len(group_images))
+            for i in range(morphed_count):
+                morphed.append(
+                    DatasetElementDescriptor(
+                        x=(group_images[i],),
+                        y=ElementClass.MORPHED,
+                    )
+                )
+        return bona_fide + morphed
