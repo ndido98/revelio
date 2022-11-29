a79e642dcef14765eaee1c498d3917cf3f668a57
ndido98 <ndido98@gmail.com>
2022-11-29 12:19:25 +0100

    feat(augmentation): add stack config to grayscale step

--- src/revelio/augmentation/grayscale.py
+++ src/revelio/augmentation/grayscale.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Any, Optional
 
 import cv2 as cv
+import numpy as np
 
 from revelio.dataset.element import Image, Landmarks
 
@@ -8,7 +9,26 @@ from .step import AugmentationStep
 
 
 class Grayscale(AugmentationStep):
+    """
+    Applies a grayscale filter to the image.
+    """
+
+    def __init__(self, *, stack: int = 1, **kwargs: Any):
+        """
+        Applies a grayscale filter to the image.
+
+        Args:
+            stack: The number of times to stack the grayscale image.
+        """
+        super().__init__(**kwargs)
+        if stack < 1:
+            raise ValueError("Stack must be positive")
+        self._stack = stack
+
     def process_element(
         self, image: Image, landmarks: Optional[Landmarks]
     ) -> tuple[Image, Optional[Landmarks]]:
-        return cv.cvtColor(image, cv.COLOR_BGR2GRAY), landmarks
+        grayscale = cv.cvtColor(image, cv.COLOR_RGB2GRAY)
+        if self._stack > 1:
+            grayscale = np.stack([grayscale] * self._stack, axis=-1)
+        return grayscale, landmarks
