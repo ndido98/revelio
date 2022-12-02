ef58d51238beeb518fe8232bcd99385123847c82
ndido98 <ndido98@gmail.com>
2022-12-02 12:03:32 +0100

    feat(preprocessing): make maximum value configurable for each channel

--- src/revelio/preprocessing/to_float.py
+++ src/revelio/preprocessing/to_float.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import numpy as np
 
 from revelio.dataset.element import ElementImage
@@ -6,9 +8,16 @@ from .step import PreprocessingStep
 
 
 class ToFloat(PreprocessingStep):
+    def __init__(self, *, max_per_channel: float | list[float] = 255.0, **kwargs: Any):
+        super().__init__(**kwargs)
+        if isinstance(max_per_channel, list):
+            self._max_per_channel = np.array(max_per_channel, dtype=np.float32)
+        else:
+            self._max_per_channel = np.array([max_per_channel], dtype=np.float32)
+
     def process_element(self, elem: ElementImage) -> ElementImage:
         if elem.image.dtype == np.uint8:
-            new_img = elem.image.astype(np.float32) / 255.0
+            new_img = elem.image.astype(np.float32) / self._max_per_channel
             return ElementImage(
                 path=elem.path,
                 image=new_img,
