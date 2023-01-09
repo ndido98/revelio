51328a3ed6bf9f0e40aab854bea90fdb83891829
ndido98 <ndido98@gmail.com>
2023-01-09 11:35:55 +0100

    feat(utils): add glob_multiple utility function

--- src/revelio/utils/files.py
+++ src/revelio/utils/files.py
@@ -16,3 +16,19 @@ def rglob_multiple(root: Path, pattern: str, extensions: Iterable[str]) -> list[
     for extension in extensions:
         result.extend(list(root.rglob(f"{pattern}.{extension}")))
     return sorted(result)
+
+
+def glob_multiple(root: Path, pattern: str, extensions: Iterable[str]) -> list[Path]:
+    """
+    Find all files matching a pattern with one of the given extensions
+    in a directory.
+
+    Args:
+        root: The root directory to search in.
+        pattern: The pattern to match.
+        extensions: The extensions to match.
+    """
+    result = []
+    for extension in extensions:
+        result.extend(list(root.glob(f"{pattern}.{extension}")))
+    return sorted(result)
